16dac4d2c2e3795819760ebeb4f56b01ea8bc083
Nghiep <me@nghiep.dev>
2024-05-17 11:05:31 +0700

    Add description

--- app/(default)/product/[id]/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function ProductLayout({children}: LayoutProps) {
-  return <>{children}</>;
-}
--- app/(default)/product/[id]/page.tsx
+++ app/(default)/product/[id]/page.tsx
@@ -1,3 +1,19 @@
+import Description from './description';
+
 export default function ProductPage() {
-  return <div>hello</div>;
+  return (
+    <>
+      <div>1</div>
+      <div>
+        <div className='flex items-start gap-x-5'>
+          <span className='block rounded-md bg-[orange] px-8 py-2 text-2xl font-semibold text-white'>
+            Description
+          </span>
+          <div className='[&_img]:inline-block'>
+            <Description />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
