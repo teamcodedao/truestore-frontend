74031b91fbbb8810328ab36ae8296242cf6f1e3c
Nghiep <me@nghiep.dev>
2024-05-19 09:14:44 +0700

    Prepare offcanvas component

---
+++ packages/ui/offcanvas/index.ts
@@ -0,0 +1,1 @@
+export {canvas as default} from './offcanvas';
---
+++ packages/ui/offcanvas/offcanvas.ts
@@ -0,0 +1,12 @@
+function showOffcanvas() {
+  console.log('showOffcanvas');
+}
+
+function closeOffcanvas() {
+  console.log('showOffcanvas');
+}
+
+export const canvas = {
+  show: showOffcanvas,
+  close: closeOffcanvas,
+};
