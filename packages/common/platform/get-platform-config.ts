08664554e07c4c2114d7241d0e063ded0ad9d95c
Nghiep <me@nghiep.dev>
2024-06-07 23:31:11 +0700

    improve: cache for platform config

--- packages/common/platform/get-platform-config.ts
+++ packages/common/platform/get-platform-config.ts
@@ -1,4 +1,5 @@
 import 'server-only';
+import {unstable_cache as cache} from 'next/cache';
 
 import ky from 'ky';
 import ms from 'ms';
@@ -8,21 +9,25 @@ import firebaseConfig from '@/config/firebase.json';
 import type {PlatformConfig} from './typings';
 import {normalizeUrl} from './utils';
 
-export async function getPlatformConfig(domain: string) {
-  const response = await ky
-    .get(
-      new URL(
-        `PXTRUE2/${normalizeUrl(domain)}/.json`,
-        firebaseConfig.clientConfig.databaseURL,
-      ),
-      {
-        next: {
-          revalidate: ms('1 day') / 1000,
-          tags: ['platform-config', 'all'],
+export const getPlatformConfig = cache(
+  async (domain: string) => {
+    const response = await ky
+      .get(
+        new URL(
+          `PXTRUE2/${normalizeUrl(domain)}/.json`,
+          firebaseConfig.clientConfig.databaseURL,
+        ),
+        {
+          cache: 'no-store',
         },
-      },
-    )
-    .json<PlatformConfig>();
+      )
+      .json<PlatformConfig>();
 
-  return response;
-}
+    return response;
+  },
+  [],
+  {
+    revalidate: ms('1 day') / 1000,
+    tags: ['platform-config', 'all'],
+  },
+);
