3fd143121bbf2ed68f232d0d42eb24dd201d5e81
Nghiep <me@nghiep.dev>
2024-06-10 16:47:27 +0700

    feat: show help modal how to find order number?

--- app/[device]/[domain]/(default)/order-tracking/page.tsx
+++ app/[device]/[domain]/(default)/order-tracking/page.tsx
@@ -5,6 +5,7 @@ import type {Metadata} from 'next';
 import {TrackPageView} from '@/components/common';
 
 import FormHandler from './form-handler';
+import HowToFind from './how-to-find';
 
 export const dynamic = 'error';
 
@@ -21,13 +22,7 @@ export default function OrderTrackingPage() {
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <FormHandler />
         <p className="mt-10 text-center text-sm text-gray-500">
-          Don&apos;t see an order number.{' '}
-          <a
-            href="#"
-            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >
-            How to find?
-          </a>
+          Don&apos;t see an order number. <HowToFind />
         </p>
       </div>
       <Suspense>
