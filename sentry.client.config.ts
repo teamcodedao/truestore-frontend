69abd070d63c80c4c8f77d4589c061f269c11076
Nghiep <me@nghiep.dev>
2024-06-12 21:57:13 +0700

    chore: add ignore errors

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -8,7 +8,39 @@ Sentry.init({
   tracesSampleRate: 1,
   environment: process.env.NODE_ENV,
   release: process.env.NEXT_PUBLIC_VERSION,
-  ignoreErrors: [...ignoreErrors, 'aborted'],
+  ignoreErrors: [
+    ...ignoreErrors,
+    'This operation was aborted',
+    'Load failed',
+    'Failed to fetch',
+    'AbortError: AbortError',
+    'SecurityError',
+    'Unknown root exit status.',
+    'Connection to Indexed Database server lost.',
+  ],
+  beforeSend(event) {
+    const userAgent = event.request?.headers?.['User-Agent'].toLowerCase();
+
+    if (
+      userAgent &&
+      [
+        'bot',
+        'crawl',
+        'spider',
+        'facebook',
+        'bingpreview',
+        'slurp',
+        'yandex',
+        'sogou',
+        'tumblr',
+        'ia_archiver',
+      ].some(word => userAgent.includes(word))
+    ) {
+      return null;
+    }
+
+    return event;
+  },
   denyUrls: [...denyUrls],
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
