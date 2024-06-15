ee76770b694900451e1685aa65ca5b9a7a5a42b1
Nghiep <me@nghiep.dev>
2024-06-15 21:49:29 +0700

    feat: add support for revalidate with path

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,10 +1,11 @@
-import {revalidateTag} from 'next/cache';
+import {revalidatePath, revalidateTag} from 'next/cache';
 import {NextRequest} from 'next/server';
 
 import {clearPlatformClient} from '@common/platform/ssr';
 
 export async function GET(request: NextRequest) {
   const tag = request.nextUrl.searchParams.get('tag');
+  const path = request.nextUrl.searchParams.get('path');
 
   if (tag) {
     revalidateTag(tag);
@@ -12,6 +13,10 @@ export async function GET(request: NextRequest) {
     if (['all', 'platform-config'].includes(tag)) {
       clearPlatformClient();
     }
+  }
+
+  if (path) {
+    revalidatePath(path);
   }
 
   return Response.json({
