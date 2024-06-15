24c9f0f10e51584770a23164a575a50db7b66c70
Nghiep <me@nghiep.dev>
2024-06-15 22:51:37 +0700

    chore: clean code

--- components/product/product-cart-mobile-actions-v0.tsx
+++ components/product/product-cart-mobile-actions-v0.tsx
@@ -3,7 +3,7 @@
 interface ProductCartMobileActionsProps {}
 
 export default function ProductCartMobileActionsV0(
-  props: ProductCartMobileActionsProps,
+  _props: ProductCartMobileActionsProps,
 ) {
   return (
     <div className="fixed bottom-0 left-0 z-[997] flex w-full gap-2 bg-[#fdfbf5] p-2">
