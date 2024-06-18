fbfd1922231e467a5066299dc8ff09336b205bc6
Nghiep <me@nghiep.dev>
2024-06-18 07:58:18 +0700

    fix: add polyfill for Intl.ListFormat

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import './bootstrap';
+import '@formatjs/intl-listformat/polyfill';
+import '@formatjs/intl-listformat/locale-data/en';
 
 export default function Providers({children}: React.PropsWithChildren) {
   return <>{children}</>;
