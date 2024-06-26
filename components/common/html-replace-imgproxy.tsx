df66c33ea7c8b312b887db5d9d99bcf05110f9ec
Nghiep <me@nghiep.dev>
2024-06-26 16:24:19 +0700

    fix: html-react-parser error

--- components/common/html-replace-imgproxy.tsx
+++ components/common/html-replace-imgproxy.tsx
@@ -37,6 +37,7 @@ export default function HtmlReplaceImgproxy({html}: HtmlReplaceImgproxyProps) {
                   src: imgproxy(src),
                 }
               : rest)}
+            style={undefined}
           />
         );
       }
