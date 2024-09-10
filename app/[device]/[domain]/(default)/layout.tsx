d854f56eb348ef97078d9595bfac33d0f29af645
NghiepDev <me@nghiep.dev>
2024-09-10 23:06:49 +0700

    style: add top banner for forest theme

--- app/[device]/[domain]/(default)/layout.tsx
+++ app/[device]/[domain]/(default)/layout.tsx
@@ -4,6 +4,9 @@ import Header from './header';
 export default function DefaultLayout({children}: LayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
+      <div className="hidden bg-black px-10 py-2 text-center text-base font-bold text-white forest:block">
+        We SHIP WORLDWIDE for all weight.
+      </div>
       <div className="container">
         <Header />
         <div>{children}</div>
