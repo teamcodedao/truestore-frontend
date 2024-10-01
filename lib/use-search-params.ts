943edc633d225dd350fe1e1f892e9c8a739942b8
NghiepDev <me@nghiep.dev>
2024-10-01 22:13:00 +0700

    Fix useSearchParams

--- lib/use-search-params.ts
+++ lib/use-search-params.ts
@@ -1,5 +1,41 @@
 import {useEffect, useState} from 'react';
 
+function createRegister() {
+  let hasBeenCalled = false;
+
+  return () => {
+    if (hasBeenCalled) {
+      return;
+    }
+    hasBeenCalled = true;
+    if (typeof window === 'undefined') {
+      return;
+    }
+    const oldPushState = history.pushState;
+    history.pushState = function pushState(...args) {
+      const ret = oldPushState.apply(this, args);
+      window.dispatchEvent(new Event('pushstate'));
+      window.dispatchEvent(new Event('locationchange'));
+      return ret;
+    };
+
+    const oldReplaceState = history.replaceState;
+    history.replaceState = function replaceState(...args) {
+      const ret = oldReplaceState.apply(this, args);
+      window.dispatchEvent(new Event('replacestate'));
+      window.dispatchEvent(new Event('locationchange'));
+      return ret;
+    };
+
+    window.addEventListener('popstate', () => {
+      window.dispatchEvent(new Event('locationchange'));
+    });
+  };
+}
+
+const register = createRegister();
+register();
+
 export function useSearchParams() {
   const [search, setSearch] = useState(() => {
     try {
@@ -10,30 +46,13 @@ export function useSearchParams() {
   });
 
   useEffect(() => {
-    (() => {
-      const oldPushState = history.pushState;
-      history.pushState = function pushState(...args) {
-        const ret = oldPushState.apply(this, args);
-        window.dispatchEvent(new Event('pushstate'));
-        window.dispatchEvent(new Event('locationchange'));
-        return ret;
-      };
-
-      const oldReplaceState = history.replaceState;
-      history.replaceState = function replaceState(...args) {
-        const ret = oldReplaceState.apply(this, args);
-        window.dispatchEvent(new Event('replacestate'));
-        window.dispatchEvent(new Event('locationchange'));
-        return ret;
-      };
-
-      window.addEventListener('popstate', () => {
-        window.dispatchEvent(new Event('locationchange'));
-      });
-    })();
-    window.addEventListener('locationchange', function () {
+    function handleLocationchange() {
       setSearch(new URLSearchParams(window.location.search));
-    });
+    }
+    window.addEventListener('locationchange', handleLocationchange);
+    return () => {
+      window.removeEventListener('locationchange', handleLocationchange);
+    };
   }, []);
 
   return search;
